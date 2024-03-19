1481fcc55294056d96651305ee5ce20674914cbc
Diego Moreno <150561693+Scout104743@users.noreply.github.com>
2024-03-19 18:01:29 -0500

    Update config.js

--- Backend/MicroProduct/src/db/config.js
+++ Backend/MicroProduct/src/db/config.js
@@ -1,3 +1,16 @@
-module.exports = {
-  db: 'mongodb://localhost:27017/baseRedes'
+const mongoose = require('mongoose');
+
+const dbConnection = async () => {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/baseRedes', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Conexión a la base de datos establecida');
+  } catch (error) {
+    console.log(error);
+    throw new Error('Error al conectar a la base de datos');
+  }
 };
+
+module.exports = dbConnection;
