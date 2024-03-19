7446c4b2355458050f00ff0cc98a967d28fff288
Diego Moreno <150561693+Scout104743@users.noreply.github.com>
2024-03-19 18:03:43 -0500

    Update config.js

--- Backend/MicroProvee/src/db/config.js
+++ Backend/MicroProvee/src/db/config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const dbConnection = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/sabortostado', {
+    await mongoose.connect('mongodb://localhost:27017/baseRedes', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
