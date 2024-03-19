8186063a7c235edbafe83747ce6d4eb5616d9fb5
StevenVegaL <stivenlopez143@gmail.com>
2024-03-19 10:38:53 -0500

    Steven ruta de usuario

--- Backend/MicroUser/src/routes/userroute.js
+++ Backend/MicroUser/src/routes/userroute.js
@@ -2,10 +2,20 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/usercontroller');
 
-// Rutas para el manejo de usuarios
-router.post('/', userController.createUser);
-router.get('/', userController.getAllUsers);
-router.put('/:userId', userController.updateUser);
-router.delete('/:userId', userController.deleteUser);
+// Obtener todos los usuarios
+router.get('/', userController.getAllUsuarios);
+
+// Obtener un usuario por su ID
+router.get('/:id', userController.getUsuarioById);
+
+// Crear un nuevo usuario
+router.post('/', userController.createUsuario);
+
+// Actualizar un usuario existente por su ID
+router.put('/:id', userController.updateUsuario);
+
+// Eliminar un usuario por su ID
+router.delete('/:id', userController.deleteUsuario);
 
 module.exports = router;
+
