d9d2be5d07fee9893e8596d6dd07a8feae6aa595
StevenVegaL <stivenlopez143@gmail.com>
2024-03-19 10:00:32 -0500

    pedidosmodels Steven

--- Backend/MicroPedidos/crs/models/pedidosmodels.js
+++ Backend/MicroPedidos/crs/models/pedidosmodels.js
@@ -1,23 +1,22 @@
 const mongoose = require('mongoose');
 
-// Esquema para la clase interna 'Detalle', que refleja la estructura interna de 'Detalle' en Java
 const detalleSchema = new mongoose.Schema({
-  proveedor_id: { type: Number, required: true }, // Asumiendo que los IDs son numéricos; ajustar si es necesario
-  producto_id: { type: Number, required: true },
-  nombreProducto: { type: String, required: true },
-  cantidad: { type: Number, required: true },
-  costePedido: { type: Number, required: true }
+    proveedor_id: { type: Number, required: true },
+    producto_id: { type: Number, required: true },
+    nombreProducto: { type: String, required: true },
+    cantidad: { type: Number, required: true },
+    costePedido: { type: Number, required: true }
 });
 
-// Esquema para 'Pedidos', incluyendo un array de 'Detalle' conforme a la definición en Java
-const pedidoSchema = new mongoose.Schema({
-  cliente_id: { type: Number, required: true }, // Este campo podría requerir ser un ObjectId o similar, dependiendo de tu diseño de base de datos
-  fechaPedido: { type: Date, required: true },
-  estadoPedido: { type: String, required: true },
-  numeroFactura: { type: Number, required: true },
-  fechaEnvio: { type: Date, required: true },
-  estadoEnvio: { type: String, required: true },
-  detalle: [detalleSchema] // Incorpora el esquema 'Detalle' como un arreglo, permitiendo múltiples entradas de detalles por pedido
-}, { collection: 'Pedidos' }); // Especifica la colección de MongoDB a la que pertenece este esquema
+const pedidosSchema = new mongoose.Schema({
+    cliente_id: { type: Number, required: true },
+    fechaPedido: { type: Date, required: true },
+    estadoPedido: { type: String, required: true },
+    numeroFactura: { type: Number, required: true },
+    fechaEnvio: { type: Date, required: false }, // Puede ser opcional dependiendo de la lógica de negocio
+    estadoEnvio: { type: String, required: true },
+    detalle: [detalleSchema] // Array de Detalles
+}, { timestamps: true }); // Agrega automáticamente campos para createdAt y updatedAt
 
-module.exports = mongoose.model('Pedido', pedidoSchema);
+module.exports = mongoose.model('Pedidos', pedidosSchema);
+
