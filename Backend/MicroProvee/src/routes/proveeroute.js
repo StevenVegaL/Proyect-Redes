8cd3a8dcde0fbb77f04aae77ba44ae10038ed82b
StevenVegaL <stivenlopez143@gmail.com>
2024-03-19 17:18:30 -0500

    Steven proveedores

--- Backend/MicroProvee/src/routes/proveeroute.js
+++ Backend/MicroProvee/src/routes/proveeroute.js
@@ -1,12 +1,1 @@
 
-const express = require('express');
-const router = express.Router();
-const { obtenerProveedores, agregarProveedor } = require('../controllers/proveecontroller');
-
-// Ruta para obtener todos los proveedores
-router.get('/', obtenerProveedores);
-
-// Ruta para agregar un nuevo proveedor
-router.post('/', agregarProveedor);
-
-module.exports = router;
