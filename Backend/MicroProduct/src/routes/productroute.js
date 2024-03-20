f23c5e69c3e950707180266806299020433eb990
StevenVegaL <stivenlopez143@gmail.com>
2024-03-20 11:14:48 -0500

    Steven

--- Backend/MicroProduct/src/routes/productroute.js
+++ Backend/MicroProduct/src/routes/productroute.js
@@ -2,10 +2,19 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productcontroller');
 
-router.post('/', productController.createProduct);
+// Obtener todos los productos
 router.get('/', productController.getProducts);
+
+// Obtener un producto por su ID
 router.get('/:id', productController.getProduct);
+
+// Crear un nuevo producto
+router.post('/', productController.createProduct);
+
+// Actualizar un producto existente por su ID
 router.patch('/:id', productController.updateProduct);
+
+// Eliminar un producto por su ID
 router.delete('/:id', productController.deleteProduct);
 
 module.exports = router;
