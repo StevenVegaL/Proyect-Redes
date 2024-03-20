d77154a03e6e0f1a8c5af31a57a55224ac39730a
StevenVegaL <stivenlopez143@gmail.com>
2024-03-20 06:31:57 -0500

    steven

--- Backend/MicroPedidos/crs/controllers/pedidoscontroller.js
+++ Backend/MicroPedidos/crs/controllers/pedidoscontroller.js
@@ -1,149 +1,127 @@
-const express = require('express');
-const router = express.Router();
-const Pedidos = require('../models/pedidosmodels'); // Ajusta la ruta al archivo de modelo
-const Pedido = require('../models/pedidosmodels');
-const { validationResult } = require('express-validator');
+const Pedido = require('../models/pedidosmodels'); // Asegúrate de que la ruta al modelo es correcta
 
-// exports.createPedido = async (req, res) => {
-//     const errors = validationResult(req);
-//     if (!errors.isEmpty()) {
-//         return res.status(400).json({ errors: errors.array() });
-//     }
-
+// const getAllPedidos = async (req, res) => {
 //     try {
-//         const newPedido = await Pedido.create(req.body);
+//         const pedidos = await Pedido.find();
+//         res.status(200).json({
+//             ok: true,
+//             pedidos,
+//         });
+//     } catch (error) {
+//         res.status(500).send(error);
+//     }
+// };
+
+const getPedidoById = async (req, res) => {
+    try {
+        const pedido = await Pedido.findById(req.params.id);
+        if (!pedido) {
+            res.status(404).send("Pedido no encontrado");
+        } else {
+            res.status(200).json({
+                ok: true,
+                pedido,
+            });
+        }
+    } catch (error) {
+        res.status(500).send(error);
+    }
+};
+
+// const createPedido = async (req, res) => {
+//     try {
+//         const nuevoPedido = new Pedido(req.body);
+//         await nuevoPedido.save();
 //         res.status(201).json({
-//             status: 'success',
-//             data: {
-//                 pedido: newPedido
-//             }
+//             ok: true,
+//             pedido: nuevoPedido,
 //         });
-//     } catch (err) {
-//         res.status(400).json({
-//             status: 'fail',
-//             message: err.message
+//     } catch (error) {
+//         res.status(400).send(error);
+//     }
+// };
+
+// const updatePedido = async (req, res) => {
+//     try {
+//         const pedidoActualizado = await Pedido.findByIdAndUpdate(req.params.id, req.body, { new: true });
+//         if (!pedidoActualizado) {
+//             res.status(404).send("Pedido no encontrado para actualizar");
+//         } else {
+//             res.status(200).json({
+//                 ok: true,
+//                 pedido: pedidoActualizado,
+//             });
+//         }
+//     } catch (error) {
+//         res.status(400).send(error);
+//     }
+// };
+
+// const deletePedido = async (req, res) => {
+//     try {
+//         const pedidoEliminado = await Pedido.findByIdAndDelete(req.params.id);
+//         if (!pedidoEliminado) {
+//             res.status(404).send("Pedido no encontrado para eliminar");
+//         } else {
+//             res.status(200).json({
+//                 ok: true,
+//                 mensaje: "Pedido eliminado",
+//             });
+//         }
+//     } catch (error) {
+//         res.status(500).send(error);
+//     }
+// };
+
+// const getPedidosPorEstadoEnvio = async (req, res) => {
+//     try {
+//         const pedidos = await Pedido.find({ estadoEnvio: req.params.estado });
+//         res.status(200).json({
+//             ok: true,
+//             pedidos,
 //         });
+//     } catch (error) {
+//         res.status(500).send(error);
+//     }
+// };
+
+// const getPedidoPorNumeroFactura = async (req, res) => {
+//     try {
+//         const pedido = await Pedido.findOne({ numeroFactura: req.params.numeroFactura });
+//         if (!pedido) {
+//             res.status(404).send("Pedido con el número de factura proporcionado no encontrado");
+//         } else {
+//             res.status(200).json({
+//                 ok: true,
+//                 pedido,
+//             });
+//         }
+//     } catch (error) {
+//         res.status(500).send(error);
+//     }
+// };
+
+// const getPedidosPorCliente = async (req, res) => {
+//     try {
+//         const pedidos = await Pedido.find({ clienteId: req.params.clienteId });
+//         res.status(200).json({
+//             ok: true,
+//             pedidos,
+//         });
+//     } catch (error) {
+//         res.status(500).send(error);
 //     }
 // };
 
 
-
-// // Middleware para obtener un pedido por ID
-// async function getPedido(req, res, next) {
-//     let pedido;
-//     try {
-//         pedido = await Pedidos.findById(req.params.id);
-//         if (pedido == null) {
-//             return res.status(404).json({ message: 'No se puede encontrar el pedido' });
-//         }
-//     } catch (err) {
-//         return res.status(500).json({ message: err.message });
-//     }
-
-//     res.pedido = pedido;
-//     next();
-// }
-
-// // Obtener todos los pedidos
-// router.get('/', async (req, res) => {
-//     try {
-//         const pedidos = await Pedidos.find();
-//         res.json(pedidos);
-//     } catch (err) {
-//         res.status(500).json({ message: err.message });
-//     }
-// });
-
-// // Obtener un pedido por ID utilizando el middleware getPedido
-// router.get('/:id', getPedido, (req, res) => {
-//     res.json(res.pedido);
-// });
-
-// // Crear un nuevo pedido
-// router.post('/', async (req, res) => {
-//     const pedido = new Pedidos(req.body);
-//     try {
-//         const nuevoPedido = await pedido.save();
-//         res.status(201).json(nuevoPedido);
-//     } catch (err) {
-//         res.status(400).json({ message: err.message });
-//     }
-// });
-
-// // Actualizar un pedido utilizando el middleware getPedido
-// router.put('/:id', getPedido, async (req, res) => {
-//     if (req.body.cliente_id != null) {
-//         res.pedido.cliente_id = req.body.cliente_id;
-//     }
-//     // Repetir para otros campos según sea necesario
-//     try {
-//         const updatedPedido = await res.pedido.save();
-//         res.json(updatedPedido);
-//     } catch (err) {
-//         res.status(400).json({ message: err.message });
-//     }
-// });
-
-// // Borrar un pedido utilizando el middleware getPedido
-// router.delete('/:id', getPedido, async (req, res) => {
-//     try {
-//         await res.pedido.remove();
-//         res.json({ message: 'Pedido eliminado' });
-//     } catch (err) {
-//         res.status(500).json({ message: err.message });
-//     }
-// });
-
-// // Consulta para obtener pedidos por el estado del envío
-// router.get('/estado-envio/:estado', async (req, res) => {
-//     try {
-//         const pedidos = await Pedidos.findByEstadoEnvio(req.params.estado);
-//         res.json(pedidos);
-//     } catch (err) {
-//         res.status(500).json({ message: err.message });
-//     }
-// });
-
-// // Obtener pedidos por ID de proveedor
-// router.get('/proveedor/:proveedorId', async (req, res) => {
-//     try {
-//         const pedidos = await Pedidos.findPedidosByProveedorId(req.params.proveedorId);
-//         res.json(pedidos);
-//     } catch (err) {
-//         res.status(500).json({ message: err.message });
-//     }
-// });
-
-// // Obtener un pedido por número de factura
-// router.get('/numero-factura/:numeroFactura', async (req, res) => {
-//     try {
-//         const pedido = await Pedidos.findByNumeroFactura(req.params.numeroFactura);
-//         if (!pedido) return res.status(404).json({ message: "Pedido con el número de factura proporcionado no encontrado" });
-//         res.json(pedido);
-//     } catch (err) {
-//         res.status(500).json({ message: err.message });
-//     }
-// });
-
-// // Obtener pedidos por fecha de pedido
-// router.get('/fecha-pedido', async (req, res) => {
-//     try {
-//         const fechaPedido = new Date(req.query.fecha); // Espera una fecha en el query string
-//         const pedidos = await Pedidos.findPedidosByFechaPedido(fechaPedido);
-//         res.json(pedidos);
-//     } catch (err) {
-//         res.status(500).json({ message: err.message });
-//     }
-// });
-
-// // Obtener pedidos por cliente ID
-// router.get('/cliente/:clienteId', async (req, res) => {
-//     try {
-//         const pedidos = await Pedidos.findPedidosByClienteId(req.params.clienteId);
-//         res.json(pedidos);
-//     } catch (err) {
-//         res.status(500).json({ message: err.message });
-//     }
-// });
-
-module.exports = router;
+module.exports = {
+    // getAllPedidos,
+    getPedidoById,
+    // createPedido,
+    // updatePedido,
+    // deletePedido,
+    // getPedidosPorEstadoEnvio,
+    // getPedidoPorNumeroFactura,
+    // getPedidosPorCliente,
+    // getVentasEnDia,
+};
--- Backend/MicroPedidos/crs/routes/pedidosroute.js
+++ Backend/MicroPedidos/crs/routes/pedidosroute.js
@@ -3,30 +3,27 @@ const router = express.Router();
 const pedidosController = require('../controllers/pedidoscontroller');
 
 // Obtener todos los pedidos
-router.get('/', pedidosController.getAllPedidos);
+// router.get('/', pedidoscontroller.getAllPedidos);
 
-// Obtener un pedido por su ID
-router.get('/:id', pedidosController.getPedidoById);
+// // Obtener un pedido por su ID
+router.get('/:id', pedidoscontroller.getPedidoById);
 
-// Crear un nuevo pedido
-router.post('/', pedidosController.createPedido);
+// // Crear un nuevo pedido
+// router.post('/', pedidosController.createPedido);
 
-// Actualizar un pedido existente por su ID
-router.put('/:id', pedidosController.updatePedido);
+// // Actualizar un pedido existente por su ID
+// router.put('/:id', pedidosController.updatePedido);
 
-// Eliminar un pedido por su ID
-router.delete('/:id', pedidosController.deletePedido);
+// // Eliminar un pedido por su ID
+// router.delete('/:id', pedidosController.deletePedido);
 
-// Obtener pedidos por estado de envío
-router.get('/estado-envio/:estado', pedidosController.getPedidosPorEstadoEnvio);
+// // Obtener pedidos por estado de envío
+// router.get('/estado-envio/:estado', pedidosController.getPedidosPorEstadoEnvio);
 
-// Obtener un pedido por número de factura
-router.get('/numero-factura/:numeroFactura', pedidosController.getPedidoPorNumeroFactura);
+// // Obtener un pedido por número de factura
+// router.get('/numero-factura/:numeroFactura', pedidosController.getPedidoPorNumeroFactura);
 
-// Obtener ventas en un día específico
-router.get('/ventas-en-dia', pedidosController.getVentasEnDia);
-
-// Obtener pedidos por ID de cliente
-router.get('/cliente/:clienteId', pedidosController.getPedidosPorCliente);
+// // Obtener pedidos por ID de cliente
+// router.get('/cliente/:clienteId', pedidosController.getPedidosPorCliente);
 
 module.exports = router;
