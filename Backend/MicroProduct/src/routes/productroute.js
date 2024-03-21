c98f40e87c2542b31bb9c42c60547fb80b0c47a1
Diego Moreno <diegofer743.mv@gmail>
2024-03-20 22:38:01 -0500

    Cambios MicroProduct

--- Backend/MicroProduct/src/routes/productroute.js
+++ Backend/MicroProduct/src/routes/productroute.js
@@ -12,7 +12,7 @@ router.get('/:id', productController.getProductById);
 router.post('/', productController.createProduct);
 
 // // Actualizar un producto existente por su ID
-// router.patch('/:id', productController.updateProduct);
+router.patch('/:id', productController.updateProduct);
 
 // // Eliminar un producto por su ID
 router.delete('/:id', productController.deleteProduct);
