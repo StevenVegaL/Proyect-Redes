d87d9b5f5363b48af927ecc7511b0cf645cb9c57
Diego Moreno <diegofer743.mv@gmail>
2024-03-21 01:23:21 -0500

    Cambios

--- Backend/MicroProvee/src/controllers/proveecontroller.js
+++ Backend/MicroProvee/src/controllers/proveecontroller.js
@@ -1,19 +1,19 @@
 const Proveedor = require('../models/proveemodel');
 
-// const createProveedor = async (req, res) => {
-//     try {
-//         const newProveedor = await Proveedor.create(req.body);
-//         res.status(201).json({
-//             ok: true,
-//             proveedor: newProveedor
-//         });
-//     } catch (err) {
-//         res.status(400).json({
-//             ok: false,
-//             error: err.message
-//         });
-//     }
-// };
+const createProveedor = async (req, res) => {
+    try {
+        const newProveedor = await Proveedor.create(req.body);
+        res.status(201).json({
+            ok: true,
+            proveedor: newProveedor
+        });
+    } catch (err) {
+        res.status(400).json({
+            ok: false,
+            error: err.message
+        });
+    }
+};
 
 const getProveedores = async (req, res) => {
     try {
@@ -31,71 +31,71 @@ const getProveedores = async (req, res) => {
     }
 };
 
-// const getProveedor = async (req, res) => {
-//     try {
-//         const proveedor = await Proveedor.findById(req.params.id);
-//         if (!proveedor) {
-//             res.status(404).json({
-//                 ok: false,
-//                 message: 'Proveedor not found'
-//             });
-//         } else {
-//             res.status(200).json({
-//                 ok: true,
-//                 proveedor
-//             });
-//         }
-//     } catch (err) {
-//         res.status(404).json({
-//             ok: false,
-//             error: err.message
-//         });
-//     }
-// };
+const getProveedor = async (req, res) => {
+    try {
+        const proveedor = await Proveedor.findById(req.params.id);
+        if (!proveedor) {
+            res.status(404).json({
+                ok: false,
+                message: 'Proveedor not found'
+            });
+        } else {
+            res.status(200).json({
+                ok: true,
+                proveedor
+            });
+        }
+    } catch (err) {
+        res.status(404).json({
+            ok: false,
+            error: err.message
+        });
+    }
+};
 
-// const updateProveedor = async (req, res) => {
-//     try {
-//         const updatedProveedor = await Proveedor.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
-//         if (!updatedProveedor) {
-//             res.status(404).json({
-//                 ok: false,
-//                 message: 'Proveedor not found'
-//             });
-//         } else {
-//             res.status(200).json({
-//                 ok: true,
-//                 proveedor: updatedProveedor
-//             });
-//         }
-//     } catch (err) {
-//         res.status(404).json({
-//             ok: false,
-//             error: err.message
-//         });
-//     }
-// };
+const updateProveedor = async (req, res) => {
+    try {
+        const updatedProveedor = await Proveedor.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+        if (!updatedProveedor) {
+            res.status(404).json({
+                ok: false,
+                message: 'Proveedor not found'
+            });
+        } else {
+            res.status(200).json({
+                ok: true,
+                proveedor: updatedProveedor
+            });
+        }
+    } catch (err) {
+        res.status(404).json({
+            ok: false,
+            error: err.message
+        });
+    }
+};
 
-// const deleteProveedor = async (req, res) => {
-//     try {
-//         await Proveedor.findByIdAndDelete(req.params.id);
-//         res.status(204).json({
-//             ok: true,
-//             message: 'Proveedor successfully deleted'
-//         });
-//     } catch (err) {
-//         res.status(404).json({
-//             ok: false,
-//             error: err.message
-//         });
-//     }
-// };
+const deleteProveedor = async (req, res) => {
+    try {
+        await Proveedor.findByIdAndDelete(req.params.id);
+        res.status(204).json({
+            ok: true,
+            message: 'Proveedor successfully deleted'
+        });
+    } catch (err) {
+        res.status(404).json({
+            ok: false,
+            error: err.message
+        });
+    }
+};
 
 module.exports = {
-    // createProveedor,
+    createProveedor,
     getProveedores,
-    // getProveedor,
-    // updateProveedor,
-    // deleteProveedor,
+    getProveedor,
+    updateProveedor,
+    deleteProveedor,
 };
 
 
