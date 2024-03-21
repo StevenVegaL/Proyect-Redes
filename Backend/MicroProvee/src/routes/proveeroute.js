7784d669cc222204750500d96f6822d25625ee64
StevenVegaL <stivenlopez143@gmail.com>
2024-03-21 16:03:41 -0500

    sateven

--- Backend/MicroProvee/src/routes/proveeroute.js
+++ Backend/MicroProvee/src/routes/proveeroute.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const proveeController = require('../controllers/proveecontroller');
 
 // router.post('/', proveeController.createProveedor);
-router.get('/', proveeController.getProveedores);
-// router.get('/:id', proveeController.getProveedor);
+// router.get('/', proveeController.getProveedores);
+router.get('/:id', proveeController.getProveedorById);
 // router.patch('/:id', proveeController.updateProveedor);
 // router.delete('/:id', proveeController.deleteProveedor);
 
