e43dda59a49ad2ed4242ae477d69c601860b94f3
StevenVegaL <stivenlopez143@gmail.com>
2024-03-20 19:21:58 -0500

    Steven

--- Backend/MicroProvee/src/models/proveemodel.js
+++ Backend/MicroProvee/src/models/proveemodel.js
@@ -1,1 +1,39 @@
+const { Schema, model } = require("mongoose");
+
+const proveedorSchema = new Schema({
+  _id: {
+    type: Number,
+    required: true
+  },
+  nombreEmpresa: {
+    type: String,
+    required: true
+  },
+  ubicacion: {
+    type: String,
+    required: true
+  },
+  telefono: {
+    type: Number,
+    required: true
+  },
+  emailProveedor: {
+    type: String,
+    required: true
+  },
+  fechaSuministro: {
+    type: Date,
+    required: true
+  },
+  productos: [{
+    type: Number,
+    required: true
+  }]
+},
+{ 
+  collection: "Proveedor" // Asegúrate de que el nombre de la colección sea el correcto
+});
+
+module.exports = model("Proveedor", proveedorSchema);
+
 
